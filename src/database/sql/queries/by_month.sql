5aec57a7cd27a970e5a1d7bc45207b784b552fda
Guilherme Gonçalves <guilherme.p.gonc@gmail.com>
2023-10-27 21:53:52 +0100

    Display account alias in ByMonth query, where available.

--- src/database/sql/queries/by_month.sql
+++ src/database/sql/queries/by_month.sql
@@ -5,7 +5,7 @@ SELECT *,
         ORDER BY 2 DESC ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING)
 FROM (
     SELECT
-        account_name,
+        IFNULL(accounts.alias, account_name),
         DATE(posted_date, "start of month", "+1 month", "-1 day") AS month,
         SUM(credit_amount) AS sum_credit_amount,
         SUM(debit_amount) AS sum_debit_amount,
@@ -14,6 +14,7 @@ FROM (
     FROM transactions
     LEFT JOIN transactions_tags ON transactions_tags.transaction_id = transactions.id
     LEFT JOIN tag_rules ON transactions_tags.tag_rule_id = tag_rules.id
+    LEFT JOIN accounts ON transactions.account_name = accounts.name
     {filters}
     GROUP BY 1, 2
     HAVING transactions.id = MAX(transactions.id) -- Last transaction of the month in account
