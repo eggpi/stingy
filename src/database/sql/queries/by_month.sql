f422bbd86a74f3c8390232dc4b585be5e34e4703
Guilherme Gonçalves <guilherme.p.gonc@gmail.com>
2024-10-27 09:50:21 +0000

    ByMonth: Don't double-count transactions that are tagged twice.

--- src/database/sql/queries/by_month.sql
+++ src/database/sql/queries/by_month.sql
@@ -1,22 +1,33 @@
-SELECT *,
-    SUM(sum_credit_amount) OVER (
-        ORDER BY 2 DESC ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING),
-    SUM(sum_debit_amount) OVER (
-        ORDER BY 2 DESC ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING)
-FROM (
+WITH filtered_unique_transactions AS (
     SELECT
-        IFNULL(accounts.alias, account_name),
+        transactions.id AS tr_id,
+        IFNULL(accounts.alias, account_name) AS account_name,
         DATE(posted_date, "start of month", "+1 month", "-1 day") AS month,
-        SUM(credit_amount) AS sum_credit_amount,
-        SUM(debit_amount) AS sum_debit_amount,
-        SUM(credit_amount) - SUM(debit_amount),
+        credit_amount,
+        debit_amount,
         balance
     FROM transactions
     LEFT JOIN transactions_tags ON transactions_tags.transaction_id = transactions.id
     LEFT JOIN tag_rules ON transactions_tags.tag_rule_id = tag_rules.id
     LEFT JOIN accounts ON transactions.account_name = accounts.name
     {filters}
+    GROUP BY transactions.id
+), aggregated_by_month AS (
+    SELECT
+        account_name,
+        month,
+        SUM(credit_amount) AS sum_credit_amount,
+        SUM(debit_amount) AS sum_debit_amount,
+        SUM(credit_amount) - SUM(debit_amount),
+        balance
+    FROM filtered_unique_transactions
     GROUP BY 1, 2
-    HAVING transactions.id = MAX(transactions.id) -- Last transaction of the month in account
+    HAVING tr_id = MAX(tr_id) -- Take the balance from the last transaction in the month
     ORDER BY 2 DESC
-);
+)
+SELECT *,
+    SUM(sum_credit_amount) OVER (
+        ORDER BY 2 DESC ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING),
+    SUM(sum_debit_amount) OVER (
+        ORDER BY 2 DESC ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING)
+FROM aggregated_by_month;
